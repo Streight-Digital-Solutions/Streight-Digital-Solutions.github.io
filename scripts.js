6692ac8d973b8b1f97f1be9335edee667b80cd12
jstreight-digital <streightdigital@gmail.com>
2026-09-21 17:18:31 -0700

    Terms updates, menu, sounds

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
+var card = document.getElementById("nav");
+card.addEventListener("mouseover", playMusic);
 
+function playMusic(){
+    var audio = new Audio("./audio/click.mp3");
+    audio.play();
+   
+}
+
+var button = document.getElementById("liability");
+button.addEventListener("click", Scroll)
+
+function Scroll(){
+    button.scrollIntoView();
+}
